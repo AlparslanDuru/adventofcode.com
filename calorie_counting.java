424d3c1b96d9796ccf2691b5c198d437c64f1ee0
AlparslanDuru <81374685+AlparslanDuru@users.noreply.github.com>
2023-01-05 23:00:57 -0600

    Update calorie_counting.java

--- calorie_counting.java
+++ calorie_counting.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class calorie_counting {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int maxCalories = 0;
